cd86b1365ae44a3a72c00c4bbda97c84cd0b603e
WilliamHong21 <78341935+WilliamHong21@users.noreply.github.com>
2022-03-02 17:48:13 +0700

    Add files via upload

---
+++ 71200617-soal1/src/main/java/org/ug4/soal1/Passenger.java
@@ -0,0 +1,15 @@
+package org.ug4.soal1;
+
+public class Passenger {
+    String name;
+    double balance;
+    String email;
+    String destiny;
+
+    public Passenger(String name, String email, double balance){
+        name = name;
+        email = email;
+        balance = balance;
+    }
+}
+
