367bd35dea74f7572ea29323e825bc025df60da2
WilliamHong21 <78341935+WilliamHong21@users.noreply.github.com>
2022-03-02 17:50:52 +0700

    Add files via upload

---
+++ 1_71200617/src/main/java/org/ug4/soal1/Bus.java
@@ -0,0 +1,16 @@
+package org.ug4.soal1;
+
+public class Bus {
+    String name;
+    String driver;
+
+    public Bus(String name, String driver){
+        name = name;
+        driver = driver;
+    }
+    String getDriver(){
+        return driver;
+    }
+
+
+}
---
+++ 1_71200617/src/main/java/org/ug4/soal1/Driver.java
@@ -0,0 +1,18 @@
+package org.ug4.soal1;
+
+public class Driver {
+    String name;
+    int age;
+    String gender;
+
+    public Driver(String name, String gender, int age){
+        name = name;
+        gender = gender;
+        age = age;
+    }
+    String getName(){
+        return name;
+    }
+
+
+}
