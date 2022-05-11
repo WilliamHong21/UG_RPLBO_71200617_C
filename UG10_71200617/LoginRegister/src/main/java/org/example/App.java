54feea69c1d05fd4d4ecabbf99279f3a75e52c86
WilliamHong21 <78341935+WilliamHong21@users.noreply.github.com>
2022-05-11 18:45:00 +0700

    Add files via upload

---
+++ UG10_71200617/LoginRegister/src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args ) {
+        String inputUsername;
+        String inputEmail;
+        String inputPassword;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Selanat datang di Toko NamaKamu");
+        System.out.println("Silahkan daftarkan diri kamu untuk dapat mengakses menu di Toko NamaKamu");
+
+        System.out.print("Username : ");
+        inputUsername = scanner.nextLine();
+
+    }
+}
